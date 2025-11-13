181d6bf017450959b94bc8c4fa1840a6d82bb0bd
Spectro <spectro@localhost.localdomain>
2025-11-13 19:30:11 +0530

    remove work files

--- fix_venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Fix venv activation - recreate it pointing to current directory
-cd "$(dirname "$0")"
-rm -rf .venv
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -e .
-echo "✓ Virtual environment recreated and package installed"
